a9503931fcedaae4af33580ed4e1919f7ca455ea
DenisBytes <denisbytes@gmail.com>
2025-10-23 17:18:10 +0200

    [feature]: add filtered and update operations

--- arangoadapter.go
+++ arangoadapter.go
@@ -305,3 +305,131 @@ func (a *Adapter) RemovePolicies(sec string, ptype string, rules [][]string) err
 	}
 	return nil
 }
+
+// RemoveFilteredPolicy removes policies that match a partial filter.
+func (a *Adapter) RemoveFilteredPolicy(sec string, ptype string, fieldIndex int, fieldValues ...string) error {
+	return a.RemoveFilteredPolicyCtx(context.Background(), sec, ptype, fieldIndex, fieldValues...)
+}
+
+// RemoveFilteredPolicyCtx is like RemoveFilteredPolicy but with context support.
+func (a *Adapter) RemoveFilteredPolicyCtx(ctx context.Context, sec string, ptype string, fieldIndex int, fieldValues ...string) error {
+	query := "FOR doc IN @@collection FILTER doc.ptype == @ptype"
+	bindVars := map[string]interface{}{
+		"@collection": a.collectionName,
+		"ptype":       ptype,
+	}
+
+	// The logic here maps the field values to the right V fields based on the starting index
+	if fieldIndex <= 0 && 0 < fieldIndex+len(fieldValues) {
+		query += " && doc.v0 == @v0"
+		bindVars["v0"] = fieldValues[0-fieldIndex]
+	}
+	if fieldIndex <= 1 && 1 < fieldIndex+len(fieldValues) {
+		query += " && doc.v1 == @v1"
+		bindVars["v1"] = fieldValues[1-fieldIndex]
+	}
+	if fieldIndex <= 2 && 2 < fieldIndex+len(fieldValues) {
+		query += " && doc.v2 == @v2"
+		bindVars["v2"] = fieldValues[2-fieldIndex]
+	}
+	if fieldIndex <= 3 && 3 < fieldIndex+len(fieldValues) {
+		query += " && doc.v3 == @v3"
+		bindVars["v3"] = fieldValues[3-fieldIndex]
+	}
+	if fieldIndex <= 4 && 4 < fieldIndex+len(fieldValues) {
+		query += " && doc.v4 == @v4"
+		bindVars["v4"] = fieldValues[4-fieldIndex]
+	}
+	if fieldIndex <= 5 && 5 < fieldIndex+len(fieldValues) {
+		query += " && doc.v5 == @v5"
+		bindVars["v5"] = fieldValues[5-fieldIndex]
+	}
+
+	query += " REMOVE doc IN @@collection"
+
+	_, err := a.db.Query(ctx, query, &arangodb.QueryOptions{
+		BindVars: bindVars,
+	})
+	return err
+}
+
+// UpdatePolicy replaces an old policy rule with a new one.
+func (a *Adapter) UpdatePolicy(sec string, ptype string, oldRule, newPolicy []string) error {
+	oldLine := a.savePolicyLine(ptype, oldRule)
+	newLine := a.savePolicyLine(ptype, newPolicy)
+
+	query := "FOR doc IN @@collection FILTER doc.ptype == @ptype"
+	bindVars := map[string]interface{}{
+		"@collection": a.collectionName,
+		"ptype":       oldLine.Ptype,
+	}
+
+	// Match the old rule
+	if oldLine.V0 != "" {
+		query += " && doc.v0 == @v0"
+		bindVars["v0"] = oldLine.V0
+	}
+	if oldLine.V1 != "" {
+		query += " && doc.v1 == @v1"
+		bindVars["v1"] = oldLine.V1
+	}
+	if oldLine.V2 != "" {
+		query += " && doc.v2 == @v2"
+		bindVars["v2"] = oldLine.V2
+	}
+	if oldLine.V3 != "" {
+		query += " && doc.v3 == @v3"
+		bindVars["v3"] = oldLine.V3
+	}
+	if oldLine.V4 != "" {
+		query += " && doc.v4 == @v4"
+		bindVars["v4"] = oldLine.V4
+	}
+	if oldLine.V5 != "" {
+		query += " && doc.v5 == @v5"
+		bindVars["v5"] = oldLine.V5
+	}
+
+	// Update it with the new values
+	query += " UPDATE doc WITH { ptype: @new_ptype, v0: @new_v0, v1: @new_v1, v2: @new_v2, v3: @new_v3, v4: @new_v4, v5: @new_v5 } IN @@collection"
+
+	bindVars["new_ptype"] = newLine.Ptype
+	bindVars["new_v0"] = newLine.V0
+	bindVars["new_v1"] = newLine.V1
+	bindVars["new_v2"] = newLine.V2
+	bindVars["new_v3"] = newLine.V3
+	bindVars["new_v4"] = newLine.V4
+	bindVars["new_v5"] = newLine.V5
+
+	_, err := a.db.Query(context.Background(), query, &arangodb.QueryOptions{
+		BindVars: bindVars,
+	})
+	return err
+}
+
+// UpdatePolicies updates multiple policy rules at once.
+func (a *Adapter) UpdatePolicies(sec string, ptype string, oldRules, newRules [][]string) error {
+	for i, oldRule := range oldRules {
+		err := a.UpdatePolicy(sec, ptype, oldRule, newRules[i])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// UpdateFilteredPolicies updates policies that match a filter.
+// Note: Currently this just adds the new policies without removing the old ones.
+// You might want to implement proper filtering logic here if you need it.
+func (a *Adapter) UpdateFilteredPolicies(sec string, ptype string, newPolicies [][]string, fieldIndex int, fieldValues ...string) ([][]string, error) {
+	oldPolicies := make([][]string, 0)
+
+	for _, newPolicy := range newPolicies {
+		err := a.AddPolicy(sec, ptype, newPolicy)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return oldPolicies, nil
+}
